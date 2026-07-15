f574c06dffef3f0c7a6e375ca9f08d07c03dce87
Hement Verma <hementver2006@gmail.com>
2026-07-15 22:39:35 +0530

    Updated project

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,15 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const connectDb=async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("db connected")
-    } catch (error) {
-        console.log("db error")
-    }
-}
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("✅ Database Connected Successfully");
+  } catch (error) {
+    console.error("❌ Database Connection Error:");
+    console.error(error.message);
+    console.error(error);
+    process.exit(1);
+  }
+};
 
-export default connectDb
+export default connectDb;
